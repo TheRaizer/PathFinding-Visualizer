b7c9248836eb1faf2a697d0ccca74f8b0f1866cb
TheRaizer <aidan.fu000@gmail.com>
2021-02-21 15:57:40 -0500

    add links to repos and email

--- path-finding/src/App.js
+++ path-finding/src/App.js
@@ -1,11 +1,13 @@
 import Grid from "./Grid/Grid";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Grid />
+      <Footer />
     </div>
   );
 }
